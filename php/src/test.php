014737af28871d8ff6bcb3735b45073a7fa16305
Carlos Thurber B <thurber.carlos@gmail.com>
2018-04-13 00:35:27 -0500

    Improved formatting of test.php output

--- php/src/test.php
+++ php/src/test.php
@@ -1,30 +1,84 @@
 <?php
 
-$output = shell_exec('./phpunit --configuration ./phpunit-internal.xml  .');
-$results = file_get_contents('./.TestResults.html');
+shell_exec('rm -f .TestResults.xml');
+shell_exec('./phpunit --configuration ./phpunit-internal.xml  .');
+
+$xml = simplexml_load_file('./.TestResults.xml');
+$RESULTS = @json_decode(@json_encode($xml ),1);
+
+$output = '';
+$suiteGlobal = $RESULTS['testsuite'];
+$attributes = $suiteGlobal['@attributes'];
+$testsuites = $suiteGlobal['testsuite'];
+
+// print globals
+// $output.="<h2>Totales</h2>";
+// $output.= json_encode($attributes);
+
+// print each file
+foreach ($testsuites as $testsuite) {
+  $attributes = $testsuite['@attributes'];
+  $testcases = $testsuite['testcase'];
+
+  $name = $attributes['name'];
+  $tests = $attributes['tests'];
+  $assertions = $attributes['assertions'];
+  $time = $attributes['time'];
+  $errors = $attributes['errors'];
+  $failures = $attributes['failures'];
+  $skipped = $attributes['skipped'];
+  $output.="<h2>$name (tests: $tests, assertions = $assertions, errors = $errors, failures = $failures, skipped = $skipped, time = $time)</h2>";
+
+  foreach ($testcases as $testcase) {
+    $attributes = $testcase['@attributes'];
+    $failure = $testcase['failure'] ?? null;
+
+    $name = fromCamelCase($attributes['name']);
+    $line = $attributes['line'];
+    $assertions = $attributes['assertions'];
+    $time = $attributes['time'];
+
+    if ($failure) {
+      $output.="<h4 class=\"failure\">❌ $name (line: $line, assertions = $assertions, time = $time)</h4>";
+      $output.= "<pre>$failure</pre>";
+    }
+    else {
+      $cssClass = $failure ? "failure" : "ok";
+      $output.="<h4 class=\"ok\">✓ $name (line: $line, assertions = $assertions, time = $time)</h4>";
+    }
+  }
+}
+
+
+function fromCamelCase($camelCaseString) {
+  $camelCaseString = str_replace('test', '', $camelCaseString);
+  $re = '/(?<=[a-z])(?=[A-Z])/x';
+  $a = preg_split($re, $camelCaseString);
+  return join($a, " " );
+}
 
 ?>
 
 <html>
 <head>
 <style>
-table, th, td {
-    border: 1px solid black;
+h4 {
+  margin-left: 30px;
+}
+pre {
+  margin-left: 100px;
+}
+.ok {
+    color: grey;
+}
+.failure {
+    color: red;
 }
 </style>
 </head>
 <body>
 
-<table style="width:100%;">
-  <tr style="width:50%">
-    <th>Results</th>
-    <th>Summary</th> 
-  </tr>
-  <tr style="width:50%">
-    <td><?php echo "<pre>$output</pre>"; ?></td>
-    <td><?php echo $results;?></td> 
-  </tr>
-</table>
+<?php echo "$output";?>
 
 </body>
 </html>
